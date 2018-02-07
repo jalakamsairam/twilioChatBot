21d1b5f54f8a8b760335a62ed3a3b71f3baf3f67
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2018-02-07 12:43:29 -0800

    changes to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var request = require("request");
 var PORT = process.env.PORT || 3001;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,4 +12,19 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
+
+app.get("/",function(req,res){
+    res.json("welcome")
+})
+app.post("/hello",function(req,res){
+    var username =  request.body.user_name;
+    var botPayLoad = {
+        text:'Hello'+username+', welcome to the channel'
+    };
+    if(username!=='slackbot'){
+        return res.status(200).json(botPayLoad);
+    }else{
+        return res.status(200).end();
+    }
+})
   

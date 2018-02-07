0cd714d1a153189f9218b8ced049040a2232ce20
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2018-02-07 12:56:52 -0800

    changes

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ var bodyParser = require("body-parser");
 var request = require("request");
 var PORT = process.env.PORT || 3001;
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
